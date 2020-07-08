60e0e344f1c95561084f173cbcf4cefa21345f66
Matthew <Mattpet26@gmail.com>
2020-07-08 16:26:32 -0700

    finalized javascript prompts

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ if (userName === 'Matthew'){
   alert('Welcome home master')
 } else {
   alert( userName + ', welcome to my website! I hope you enjoy it!')
+
 }
+console.log(userName)
 
 var humanAnswer = prompt('Is Matthew human?');
 
@@ -13,9 +15,9 @@ if (humanAnswer.toLowerCase() === 'yes'){
   alert('Correct!');
 } if (humanAnswer.toLowerCase() === 'no'){
   alert('Beep Boop Beep, how did you know?');
-} else {
-  alert('That is an invalid answer.')
-}
+  
+} 
+console.log(humanAnswer)
 
 var fishAnswer = prompt('How many fish do I have?');
 
@@ -34,7 +36,9 @@ case 'three':
   break;
 default:
   alert('Not even close!');
+  
 }
+console.log(fishAnswer)
 
 var instrumentAnswer = prompt('Can I play any instruments?');
 
@@ -42,7 +46,9 @@ if (instrumentAnswer.toLowerCase() === 'yes'){
   alert('I can play the piano!');
 } else {
   alert('I can play one or two, not well though!');
+  
 }
+console.log(instrumentAnswer)
 
 var seattleAnswer = prompt('Am I from Seattle?');
 
@@ -50,7 +56,9 @@ if (seattleAnswer.toLowerCase() === 'yes'){
    alert('Nope! I\'m actually from a different state.');
  } else {
    alert('I\'m from Wisconsin!');
+  
 }
+console.log(seattleAnswer)
 
 var codingAnswer = prompt('Does Matthew enjoy coding?')
 
@@ -58,7 +66,9 @@ if (codingAnswer.toLowerCase() === 'yes'){
   alert('You are somewhat correct!')
 } else {
   alert('You are somewhat incorrect!')
+  
 }
+console.log(codingAnswer)
 
 for (var i = 0; i < 4; i = i + 1){
   var numberGuessed = prompt('Guess a number between 1 and 10?')
@@ -67,39 +77,57 @@ for (var i = 0; i < 4; i = i + 1){
       break;
   } else if (numberGuessed === '1') {
       alert('You are so close, just 1 off!');
-  } else if (numberGuessed >= '3'){
-      alert('Sorry, but thats too high.');
-  } else if(numberGuessed !== '2' && i === 3){
+  } else if (numberGuessed === '3'){
+      alert('Just a little too high.');
+  } else if (numberGuessed > '4'){
+      alert('Try a much smaller number!.');
+  } if(numberGuessed !== '2' && i === 3){
       alert('The correct answer was 2, nice try!')
+      
   }
 }
+console.log(numberGuessed)
+
+var guessingArray = ['red', 'blue', 'green'];
+guessingArray[0] === 'red';
+guessingArray[0];
+
+guessingArray[1] === 'blue';
+guessingArray[1];
+
+guessingArray[2] === 'green';
+guessingArray[2];
 
 for (var i = 0; i < 6; i = i + 1){
-  var guessingGame = prompt('Try and guess my middle name');
-  if(guessingGame ===guessingArray[0]){
+var guessingGame = prompt('Try guessing one of my desktops colors!');
+
+  if(guessingGame === guessingArray[0] || guessingGame === guessingArray[2] || guessingGame === guessingArray[1]){
     alert('Woa you must be a gene!')
+    break;
+  } else if(guessingGame !== guessingArray[0] || guessingGame !== guessingArray[2] || guessingGame !== guessingArray[1]) {
+    alert('Sorry that was incorrect, try a different color.')
+  } if(guessingGame !== guessingArray[0] && i === 5){
+    alert('The correct answers were red, blue or green!')
+    
   }
+}
+console.log(guessingGame)
 
-  guessingArray[0] === 'Logan';
-  guessingArray[0];
+var count = 0
+if (humanAnswer.toLowerCase() === 'yes'){
+  count++;
+} if (codingAnswer.toLowerCase() === 'yes'){
+  count++;
+} if (numberGuessed === '2'){
+  count++;
+} if(guessingGame === guessingArray[0] || guessingGame === guessingArray[2] || guessingGame === guessingArray[1]){
+  count++;
+} if (instrumentAnswer.toLowerCase() === 'yes'){
+  count++;
+} if (seattleAnswer.toLowerCase() === 'no'){
+  count++;
+}
 
-  guessingArray[1] === 'Liam';
-  guessingArray[1];
-
-  guessingArray[2] === 'Lewis';
-  guessingArray[2];
-
-  guessingArray[3] === 'Lebron';
-  guessingArray[3];
-
-  guessingArray[4] === 'Lance';
-  guessingArray[4];
-
-  guessingArray[5] === 'Larson';
-  guessingArray[5];
-
-
+var correctAnswers = alert('You got ' + count + ' questions correct! Way to go!')
 
 alert('Thank you ' + userName + ' for answering my questions! I appreciate your time!')
-
-forceTest();
