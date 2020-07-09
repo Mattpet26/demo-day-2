e01fa20eadb498970ca2ae303cee9799243b3bd0
Matthew <Mattpet26@gmail.com>
2020-07-09 16:21:17 -0700

    fixxed question counter

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-'use strict';
+var score = 0;
 
 function asknamequestion(){
 var userName = prompt ('Please enter your name so we know who we are talking with.')
@@ -13,19 +13,19 @@ if (userName === 'Matthew'){
 }
 asknamequestion();
 
-
 function askhumanquestion(){
 var humanAnswer = prompt('Is Matthew human?');
 if (humanAnswer.toLowerCase() === 'yes'){
   alert('Correct!');
+  score = score + 1;
 } if (humanAnswer.toLowerCase() === 'no'){
   alert('Beep Boop Beep, how did you know?');
   console.log(humanAnswer)
 } 
 }
+askhumanquestion();
 
 function askfishquestion(){
-
 var fishAnswer = prompt('How many fish do I have?');
 
 switch(fishAnswer){
@@ -40,52 +40,54 @@ case 'two':
   break;
 case 'three':
   alert('Correct!');
+  score = score + 1;
   break;
 default:
   alert('Not even close!');
-  
+  console.log(fishAnswer)
 }
 }
 askfishquestion();
-// console.log(fishAnswer)
-function askinstrumentquestion(){
 
+function askinstrumentquestion(){
 var instrumentAnswer = prompt('Can I play any instruments?');
 
 if (instrumentAnswer.toLowerCase() === 'yes'){
   alert('I can play the piano!');
+  score = score + 1;
 } else {
   alert('I can play one or two, not well though!');
-  
+  console.log(instrumentAnswer)
 }
 }
 askinstrumentquestion();
-// console.log(instrumentAnswer)
-function askseattlequestion(){
 
+function askseattlequestion(){
 var seattleAnswer = prompt('Am I from Seattle?');
 
 if (seattleAnswer.toLowerCase() === 'yes'){
    alert('Nope! I\'m actually from a different state.');
  } else {
    alert('I\'m from Wisconsin!');
-  
+   score = score + 1;
+   console.log(seattleAnswer)
 }
 }
 askseattlequestion();
-// console.log(seattleAnswer)
-function askcodingquestion(){
 
+function askcodingquestion(){
 var codingAnswer = prompt('Does Matthew enjoy coding?')
 
 if (codingAnswer.toLowerCase() === 'yes'){
   alert('You are somewhat correct!');
+  score = score + 1;
 } else {
   alert('You are somewhat incorrect!');
+  console.log(codingAnswer)
 } 
 }
 askcodingquestion();
-// console.log(codingAnswer)
+
 
 function asknumberquestion(){
 
@@ -93,6 +95,7 @@ for (var i = 0; i < 4; i = i + 1){
   var numberGuessed = prompt('Guess a number between 1 and 10?')
     if (numberGuessed === '2'){
       alert('How\'d you know, wizard??');
+      score = score + 1;
       break;
   } else if (numberGuessed === '1') {
       alert('You are so close, just 1 off!');
@@ -102,15 +105,13 @@ for (var i = 0; i < 4; i = i + 1){
       alert('Try a much smaller number!.');
   } if(numberGuessed !== '2' && i === 3){
       alert('The correct answer was 2, nice try!')
-      
+      console.log(numberGuessed)
   }
 }
 }
 asknumberquestion();
-// console.log(numberGuessed)
 
 function askarray(){
-
 var guessingArray = ['red', 'blue', 'green'];
 
 for (var i = 0; i < 6; i++){
@@ -118,34 +119,19 @@ var guessingGame = prompt('Try guessing one of my desktops colors!');
 
   if(guessingGame === guessingArray[0] || guessingGame === guessingArray[2] || guessingGame === guessingArray[1]){
     alert('Woa you must be a gene!');
+    score = score + 1;
     break;
   } else if(guessingGame !== guessingArray[0] || guessingGame !== guessingArray[2] || guessingGame !== guessingArray[1]) {
     alert('Sorry that was incorrect, try a different color.');
   } if(guessingGame !== guessingArray[0] && i === 5){
     alert('The correct answers were red, blue or green!');
-    
+    console.log(guessingGame)
   }
 }
 }
 askarray();
-console.log(guessingGame)
 
-// var count = 0
-// if (humanAnswer.toLowerCase() === 'yes'){
-//   count++;
-// } if (codingAnswer.toLowerCase() === 'yes'){
-//   count++;
-// } if (numberGuessed === '2'){
-//   count++;
-// } if(guessingGame === guessingArray[0] || guessingGame === guessingArray[2] || guessingGame === guessingArray[1]){
-//   count++;
-// } if (instrumentAnswer.toLowerCase() === 'yes'){
-//   count++;
-// } if (seattleAnswer.toLowerCase() === 'no'){
-//   count++;
-// }
-// var correctAnswers = alert('You got ' + count + ' questions correct! Way to go!');
+
+alert('You got ' + score + ' questions correct! Way to go!');
 
 alert('Thank you ' + userName + ' for answering my questions! I appreciate your time!');
-
-askhumanquestion();
