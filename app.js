c9c979142ab88d163967b79a9e3c0afdc19e05d9
john ramirez <jackrunners317@gmail.com>
2020-07-09 14:05:10 -0700

    wrapped fish question and removed console log error.

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-function askUserName(){
+function asknamequestion(){
 var userName = prompt ('Please enter your name so we know who we are talking with.')
 if (userName === 'Matthew'){
   alert('Welcome home master')
@@ -9,12 +9,11 @@ if (userName === 'Matthew'){
 
 }
 }
-askUserName();
-console.log(userName)
+asknamequestion();
+// console.log(userName)
 
-function askHumanAnswer(){
+function askhumanquestion(){
 var humanAnswer = prompt('Is Matthew human?');
-
 if (humanAnswer.toLowerCase() === 'yes'){
   alert('Correct!');
 } if (humanAnswer.toLowerCase() === 'no'){
@@ -22,8 +21,10 @@ if (humanAnswer.toLowerCase() === 'yes'){
   
 } 
 }
-askHumanAnswer();
-console.log(humanAnswer)
+askhumanquestion();
+// console.log(humanAnswer)
+
+function askfishquestion(){
 
 var fishAnswer = prompt('How many fish do I have?');
 
@@ -35,7 +36,7 @@ case 'one':
   alert('Getting closer');
   break;
 case 'two':
-  alert('Getting warmer')
+  alert('Getting warmer');
   break;
 case 'three':
   alert('Correct!');
@@ -44,88 +45,90 @@ default:
   alert('Not even close!');
   
 }
-console.log(fishAnswer)
-
-var instrumentAnswer = prompt('Can I play any instruments?');
-
-if (instrumentAnswer.toLowerCase() === 'yes'){
-  alert('I can play the piano!');
-} else {
-  alert('I can play one or two, not well though!');
-  
 }
-console.log(instrumentAnswer)
+askfishquestion();
+// console.log(fishAnswer)
 
-var seattleAnswer = prompt('Am I from Seattle?');
+// var instrumentAnswer = prompt('Can I play any instruments?');
 
-if (seattleAnswer.toLowerCase() === 'yes'){
-   alert('Nope! I\'m actually from a different state.');
- } else {
-   alert('I\'m from Wisconsin!');
+// if (instrumentAnswer.toLowerCase() === 'yes'){
+//   alert('I can play the piano!');
+// } else {
+//   alert('I can play one or two, not well though!');
   
-}
-console.log(seattleAnswer)
+// }
+// console.log(instrumentAnswer)
 
-var codingAnswer = prompt('Does Matthew enjoy coding?')
+// var seattleAnswer = prompt('Am I from Seattle?');
 
-if (codingAnswer.toLowerCase() === 'yes'){
-  alert('You are somewhat correct!')
-} else {
-  alert('You are somewhat incorrect!')
+// if (seattleAnswer.toLowerCase() === 'yes'){
+//    alert('Nope! I\'m actually from a different state.');
+//  } else {
+//    alert('I\'m from Wisconsin!');
   
-}
-console.log(codingAnswer)
+// }
+// console.log(seattleAnswer)
 
-for (var i = 0; i < 4; i = i + 1){
-  var numberGuessed = prompt('Guess a number between 1 and 10?')
-    if (numberGuessed === '2'){
-      alert('How\'d you know, wizard??');
-      break;
-  } else if (numberGuessed === '1') {
-      alert('You are so close, just 1 off!');
-  } else if (numberGuessed === '3'){
-      alert('Just a little too high.');
-  } else if (numberGuessed > '4'){
-      alert('Try a much smaller number!.');
-  } if(numberGuessed !== '2' && i === 3){
-      alert('The correct answer was 2, nice try!')
+// var codingAnswer = prompt('Does Matthew enjoy coding?')
+
+// if (codingAnswer.toLowerCase() === 'yes'){
+//   alert('You are somewhat correct!')
+// } else {
+//   alert('You are somewhat incorrect!')
+  
+// }
+// console.log(codingAnswer)
+
+// for (var i = 0; i < 4; i = i + 1){
+//   var numberGuessed = prompt('Guess a number between 1 and 10?')
+//     if (numberGuessed === '2'){
+//       alert('How\'d you know, wizard??');
+//       break;
+//   } else if (numberGuessed === '1') {
+//       alert('You are so close, just 1 off!');
+//   } else if (numberGuessed === '3'){
+//       alert('Just a little too high.');
+//   } else if (numberGuessed > '4'){
+//       alert('Try a much smaller number!.');
+//   } if(numberGuessed !== '2' && i === 3){
+//       alert('The correct answer was 2, nice try!')
       
-  }
-}
-console.log(numberGuessed)
+//   }
+// }
+// console.log(numberGuessed)
 
-var guessingArray = ['red', 'blue', 'green'];
+// var guessingArray = ['red', 'blue', 'green'];
 
-for (var i = 0; i < 6; i++){
-var guessingGame = prompt('Try guessing one of my desktops colors!');
+// for (var i = 0; i < 6; i++){
+// var guessingGame = prompt('Try guessing one of my desktops colors!');
 
-  if(guessingGame === guessingArray[0] || guessingGame === guessingArray[2] || guessingGame === guessingArray[1]){
-    alert('Woa you must be a gene!')
-    break;
-  } else if(guessingGame !== guessingArray[0] || guessingGame !== guessingArray[2] || guessingGame !== guessingArray[1]) {
-    alert('Sorry that was incorrect, try a different color.')
-  } if(guessingGame !== guessingArray[0] && i === 5){
-    alert('The correct answers were red, blue or green!')
+//   if(guessingGame === guessingArray[0] || guessingGame === guessingArray[2] || guessingGame === guessingArray[1]){
+//     alert('Woa you must be a gene!')
+//     break;
+//   } else if(guessingGame !== guessingArray[0] || guessingGame !== guessingArray[2] || guessingGame !== guessingArray[1]) {
+//     alert('Sorry that was incorrect, try a different color.')
+//   } if(guessingGame !== guessingArray[0] && i === 5){
+//     alert('The correct answers were red, blue or green!')
     
-  }
-}
-console.log(guessingGame)
+//   }
+// }
+// console.log(guessingGame)
 
-var count = 0
-if (humanAnswer.toLowerCase() === 'yes'){
-  count++;
-} if (codingAnswer.toLowerCase() === 'yes'){
-  count++;
-} if (numberGuessed === '2'){
-  count++;
-} if(guessingGame === guessingArray[0] || guessingGame === guessingArray[2] || guessingGame === guessingArray[1]){
-  count++;
-} if (instrumentAnswer.toLowerCase() === 'yes'){
-  count++;
-} if (seattleAnswer.toLowerCase() === 'no'){
-  count++;
-}
+// var count = 0
+// if (humanAnswer.toLowerCase() === 'yes'){
+//   count++;
+// } if (codingAnswer.toLowerCase() === 'yes'){
+//   count++;
+// } if (numberGuessed === '2'){
+//   count++;
+// } if(guessingGame === guessingArray[0] || guessingGame === guessingArray[2] || guessingGame === guessingArray[1]){
+//   count++;
+// } if (instrumentAnswer.toLowerCase() === 'yes'){
+//   count++;
+// } if (seattleAnswer.toLowerCase() === 'no'){
+//   count++;
+// }
 
-var correctAnswers = alert('You got ' + count + ' questions correct! Way to go!')
+// var correctAnswers = alert('You got ' + count + ' questions correct! Way to go!')
 
-alert('Thank you ' + userName + ' for answering my questions! I appreciate your time!')
+// alert('Thank you ' + userName + ' for answering my questions! I appreciate your time!')
